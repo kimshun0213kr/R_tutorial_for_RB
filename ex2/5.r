4c17b903809c83237c4cfdfbd73323da5b1abe43
kimshunKR <96727208+kimshun0213kr@users.noreply.github.com>
2023-02-20 18:38:56 +0900

    Delete ex2 directory

--- ex2/5.r
+++
@@ -1,2 +0,0 @@
-mat1 <- matrix(c(1:6),byrow = FALSE)
-mat2 <- matrix(1:12,c(3,4),byrow = TRUE)
--- ex2/pass.py
+++
@@ -1,8 +0,0 @@
-from random import randint as rd
-num = [1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","@","_","$"]
-
-for i in range(12):
-    res = rd(0,len(num)-1)
-    print(num[res],end="")
-
-print("\n")
