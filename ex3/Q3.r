9c9098dfcc2d584e4d90090f9ae1b89c7755a293
kimshunKR <96727208+kimshun0213kr@users.noreply.github.com>
2023-02-20 18:39:12 +0900

    Delete ex3 directory

--- ex3/Q3.r
+++
@@ -1,2 +0,0 @@
-Mat_Q3_1 <- matrix(c(13:16),nrow = 2,ncol = 2,byrow = TRUE)
-Mat_Q3_2 <- solve(Mat_Q3_1)
--- ex3/master.r
+++
@@ -1,2 +0,0 @@
-mat1 <- matrix(c(1:9),nrow = 3,ncol = 3)
-mat2 <- matrix(c(10:18),nrow = 3,ncol = 3)
